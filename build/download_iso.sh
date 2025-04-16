15f6331db2715cd1c7f04ef1cbc27fbba15e9726
The Greg <greg@border0.com>
2025-04-16 13:59:48 -0700

    ME-4583: ADD buiild directory and scripts, update typos in webUI

---
+++ build/download_iso.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# download the iso image
+curl https://downloads.raspberrypi.org/raspios_lite_arm64/images/raspios_lite_arm64-2024-01-17/2024-01-16-raspios-bookworm-arm64-lite.img.xz -o ../iso/2024-01-16-raspios-bookworm-arm64-lite.img.xz
+
