df5f52e93b5d05298c4b9aa91198d574089386c0
The Greg <greg@border0.com>
2025-05-22 13:02:23 +0200

    FEAT: move the WebUI to port 80

--- webui/webui.py
+++ webui/webui.py
@@ -5,4 +5,4 @@ from gateway_admin.app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 80)))
